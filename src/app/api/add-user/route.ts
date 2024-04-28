70c95cbbf474700471f12b35e7bb3277853553ee
dscott004 <danscottlearns@gmail.com>
2024-04-27 22:29:16 -0600

    Used app router format instead of pages router

--- src/app/api/add-user/route.ts
+++ src/app/api/add-user/route.ts
@@ -1,31 +1,51 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
+import { NextResponse } from 'next/server';
 import setupDatabase from '../../../lib/db';
 import { v4 as uuidv4 } from 'uuid';
 
 interface ApiResponse {
- success: boolean;
- token?: string;
- player?: any;
- message?: string;
- error?: any;
+  success: boolean;
+  token?: string;
+  player?: any;
+  message?: string;
+  error?: any;
 }
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse<ApiResponse>) {
- const { name } = req.body;
- const token = uuidv4();
- const expiry = new Date();
- expiry.setDate(expiry.getDate() + 1); // Set expiry to 24 hours later
+export async function POST(req: Request) {
+  try {
+    const { name } = await req.json();
+    const token = uuidv4();
+    const expiry = new Date();
+    expiry.setDate(expiry.getDate() + 1); // Set expiry to 24 hours later
 
- try {
-   const db = await setupDatabase();
-   const player = await db.run(
-     `INSERT INTO Players (Name, LastLogin, LastModified) VALUES (?, datetime('now'), datetime('now')) ON CONFLICT (Name) DO UPDATE SET LastLogin = datetime('now'), LastModified = datetime('now')`,
-     [name]
-   );
+    const db = await setupDatabase();
+    const player = await db.run(
+      `INSERT INTO Players (Name, LastLogin, LastModified)
+      VALUES (?, datetime('now'), datetime('now'))
+      ON CONFLICT (Name) DO UPDATE SET
+        LastLogin = datetime('now'),
+        LastModified = datetime('now')`,
+      [name]
+    );
 
-   res.setHeader('Set-Cookie', `token=${token}; Expires=${expiry.toUTCString()}; Path=/`);
-   res.status(200).json({ success: true, token, player });
- } catch (error) {
-   res.status(500).json({ success: false, message: 'Database error', error });
- }
+    const response: ApiResponse = {
+      success: true,
+      token,
+      player,
+    };
+
+    const headers = {
+      'Set-Cookie': `token=${token}; Expires=${expiry.toUTCString()}; Path=/`,
+      'Content-Type': 'application/json',
+    };
+
+    return new NextResponse(JSON.stringify(response), { status: 200, headers });
+  } catch (error) {
+    const errorResponse: ApiResponse = {
+      success: false,
+      message: 'Database error',
+      error,
+    };
+
+    return new NextResponse(JSON.stringify(errorResponse), { status: 500 });
+  }
 }
