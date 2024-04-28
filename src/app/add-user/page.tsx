2cbef8cdf68c214f75332e41955b2158f36cf4d1
dscott004 <danscottlearns@gmail.com>
2024-04-27 22:40:33 -0600

    Uses  next/navigation instetad of next/router

--- src/app/add-user/page.tsx
+++ src/app/add-user/page.tsx
@@ -1,32 +1,27 @@
 'use client';
-import React, { useEffect } from 'react';
-import { useRouter } from 'next/router';
+import React from 'react';
+import { useRouter } from 'next/navigation';
 
 export default function AddUser() {
   const router = useRouter();
 
-  useEffect(() => {
-    // Only run the effect if the router is ready
-    if (!router.isReady) return;
+  const fetchUser = async () => {
+    const response = await fetch('/api/add-user', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ name: 'Your Name' }),
+    });
 
-    const fetchUser = async () => {
-      const response = await fetch('/api/add-user', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ name: 'Your Name' }),
-      });
+    const data = await response.json();
+    if (data.success) {
+      console.log('User added/updated', data);
+      router.push('/lobby');
+    }
+  };
 
-      const data = await response.json();
-      if (data.success) {
-        console.log('User added/updated', data);
-        router.push('/lobby');
-      }
-    };
-
-    fetchUser();
-  }, [router]); // 
+  fetchUser();
 
   return (
     <div>
