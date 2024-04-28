d4ad0cee0a59165cea775d0c39cb3b6b7cd2522a
dscott004 <danscottlearns@gmail.com>
2024-04-27 22:34:57 -0600

    Updated how db is created

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,29 +1,11 @@
-
 import { open } from 'sqlite';
 import sqlite3 from 'sqlite3';
 
 async function setupDatabase() {
-    const db = await open({
+    return await open({
         filename: 'mydatabase.db',
         driver: sqlite3.Database
     });
-
-    await db.exec(`
-        CREATE TABLE IF NOT EXISTS Players (
-            Id INTEGER PRIMARY KEY AUTOINCREMENT,
-            DateCreated TEXT DEFAULT (datetime('now')),
-            Name TEXT NOT NULL UNIQUE,
-            LastLogin TEXT DEFAULT (datetime('now')),
-            OverallScore INTEGER DEFAULT 0,
-            Wins INTEGER DEFAULT 0,
-            Losses INTEGER DEFAULT 0,
-            GameCount INTEGER DEFAULT 0,
-            LastModified TEXT DEFAULT (datetime('now')),
-            CurrentGame INTEGER
-        )
-    `);
-
-    return db;
 }
 
 export default setupDatabase;
--- src/scripts/createPlayerTable.ts
+++ src/scripts/createPlayerTable.ts
@@ -1,22 +1,24 @@
-import db from '../lib/db.ts';
+import setupDatabase from '../lib/db';
 
 const createPlayersTable = async (): Promise<void> => {
-  if (!await db.schema.hasTable('Players')) {
-    await db.schema.createTable('Players', table => {
-      table.increments('Id').primary();
-      table.timestamp('DateCreated').defaultTo(db.fn.now());
-      table.string('Name').notNullable();
-      table.timestamp('LastLogin').defaultTo(db.fn.now());
-      table.integer('OverallScore').defaultTo(0);
-      table.integer('Wins').defaultTo(0);
-      table.integer('Losses').defaultTo(0);
-      table.integer('GameCount').defaultTo(0);
-      table.timestamp('LastModified').defaultTo(db.fn.now());
-      table.integer('CurrentGame').nullable();
-    });
-  
-    console.log('Players table created');
-  }
+  const db = await setupDatabase();
+  await db.exec(`
+    CREATE TABLE IF NOT EXISTS Players (
+      Id INTEGER PRIMARY KEY AUTOINCREMENT,
+      DateCreated TEXT DEFAULT (datetime('now')),
+      Name TEXT NOT NOT NULL UNIQUE,
+      LastLogin TEXT DEFAULT (datetime('now')),
+      OverallScore INTEGER DEFAULT 0,
+      Wins INTEGER DEFAULT 0,
+      Losses INTEGER DEFAULT 0,
+      GameCount INTEGER DEFAULT 0,
+      LastModified TEXT DEFAULT (datetime('now')),
+      CurrentGame INTEGER
+    )
+  `);
+
+  console.log('Players table created');
+  await db.close();
 };
 
 createPlayersTable().catch(err => {
