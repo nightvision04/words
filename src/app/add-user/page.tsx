5caecf642afe5130b3efcd14d29a01fb4b6c5769
dscott004 <danscottlearns@gmail.com>
2024-04-28 00:02:48 -0600

    Conditionallly fire the add-user featchUser function

--- src/app/add-user/page.tsx
+++ src/app/add-user/page.tsx
@@ -1,28 +1,38 @@
 'use client';
-import React from 'react';
+import React, { useEffect, useCallback } from 'react';
 import { useRouter } from 'next/navigation';
 
 export default function AddUser() {
   const router = useRouter();
   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || '';
 
-  const fetchUser = async () => {
-    const response = await fetch(`${baseUrl}/api/add-user`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ name: 'Your Name' }),
-    });
+  const fetchUser = useCallback(async () => {
+    try {
+      const response = await fetch(`${baseUrl}/api/add-user`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name: 'Your Name' }),
+      });
 
-    const data = await response.json();
-    if (data.success) {
-      console.log('User added/updated', data);
-      router.push('/lobby');
+      const data = await response.json();
+      if (data.success) {
+        console.log('User added/updated', data);
+        router.push('/lobby');
+      } else {
+        console.error('Error adding/updating user:', data.message);
+      }
+    } catch (error) {
+      console.error('Error adding/updating user:', error);
     }
-  };
+  }, [baseUrl, router]);
 
-  fetchUser();
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      fetchUser();
+    }
+  }, [fetchUser]);
 
   return (
     <div>
